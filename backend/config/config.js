ce6fd9fd566967b6227dfa578aaa9b5ea079ff74
ssladek <stephan.sladek@nolimit-it.de>
2015-12-06 09:15:40 +0100

    remove

--- backend/config/config.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  module.exports = function() {
-    var env;
-    env = process.env.NODE_ENV || 'development';
-    return env;
-  };
-
-}).call(this);
--- backend/pdf/pdf.service.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  exports.createPDF = function(appUser, implantId, operationId, filePathAndName, callback) {
-    return callback("Bis Mitte Dezember nicht unterstützt");
-  };
-
-}).call(this);
